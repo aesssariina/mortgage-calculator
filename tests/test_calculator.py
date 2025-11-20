9c805f3373e4c82d9e79c47869dbf0c57f6dab2d
Арина <arinakis62@gmail.com>
2025-11-20 18:23:46 +0700

    test17

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,27 +2,37 @@ import unittest
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from calculator import MortgageCalculator
+from calculator import SimpleCalculator
 
-
-class TestMortgageCalculator(unittest.TestCase):
+class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
-        self.calc = MortgageCalculator()
+        self.calc = SimpleCalculator()
 
-    def test_monthly_payment(self):
-        payment = self.calc.calculate_monthly_payment(1000000, 7.5, 20)
-        self.assertAlmostEqual(payment, 8055.93, places=2)
+    def test_add(self):
+        self.assertEqual(self.calc.add(7, 2), 9)
+        self.assertEqual(self.calc.add(3, -2), 1)
 
-    def test_total_payment(self):
-        total = self.calc.calculate_total_payment(1000000, 20)
-        self.assertAlmostEqual(total, 1933423.2, places=2)
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(12, 4), 8)
+        self.assertEqual(self.calc.subtract(3, 7), -4)
 
-    def test_total_interest(self):
-        interest = self.calc.calculate_total_interest(1000000, 7.5, 20)
-        self.assertAlmostEqual(interest, 933423.2, places=2)
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 7), 14)
+        self.assertEqual(self.calc.multiply(0, 4), 0)
 
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(22, 2), 11)
+        self.assertAlmostEqual(self.calc.divide(11, 2), 5.5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            self.calc.divide(6, 0)
+
+    def test_power(self):
+        self.assertEqual(self.calc.power(1, 3), 1)
+        self.assertEqual(self.calc.power(7, 0), 1)
 
 if __name__ == '__main__':
     unittest.main()
