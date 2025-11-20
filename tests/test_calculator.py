b54ae6a1595546eda510add448b3847bbd9a6a09
Арина <arinakis62@gmail.com>
2025-11-20 17:56:15 +0700

    test10

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -16,12 +16,12 @@ class TestMortgageCalculator(unittest.TestCase):
         self.assertAlmostEqual(payment, 8055.93, places=2)
 
     def test_total_payment(self):
-        total = self.calc.calculate_total_payment(1000000, 7.5, 20)
+        total = self.calc.calculate_total_payment(1000000, 20)
         self.assertAlmostEqual(total, 1933423.2, places=2)
 
     def test_total_interest(self):
         interest = self.calc.calculate_total_interest(1000000, 7.5, 20)
-        self.assertAlmostEqual(interest, 933423.2, places=0)
+        self.assertAlmostEqual(interest, 933423.2, places=2)
 
 
 if __name__ == '__main__':
